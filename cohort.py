6b439382844028ea58a2ab530e3e712c12404398
Harostar01 <joshuaolamilekan008@gmail.com>
2024-09-28 13:25:31 +0100

    new push

---
+++ cohort.py
@@ -0,0 +1,10 @@
+go = 1234.5
+we = 2334.5
+you = 4321.7
+Me =  int (3434.6) 
+see = int (5667.0)
+# Here we want to add up 
+add_up = go+we+you+Me+see
+# Here we want to divide by 2
+divide_2 = add_up/2
+print(divide_2)
